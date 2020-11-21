e02c82f83eebf4557669bd3488e364cd5dfb45d7
joostvangils <54207730+joostvangils@users.noreply.github.com>
2020-11-21 12:08:48 +0100

    Create Standard_returns.py

---
+++ BPMN_RPA/Scripts/Standard_returns.py
@@ -0,0 +1,17 @@
+
+def return_true():
+    return True
+
+
+def return_false():
+    return False
+
+
+def output_ok():
+    print(0)
+    return 0
+
+
+def output_not_ok():
+    print(-1)
+    return -1
