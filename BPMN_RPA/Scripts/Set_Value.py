03012a479bffdb2010ffa8fe6e39dd3dde1cb5b3
joostvangils <54207730+joostvangils@users.noreply.github.com>
2020-11-21 12:06:44 +0100

    Update Set_Value.py

--- BPMN_RPA/Scripts/Set_Value.py
+++ BPMN_RPA/Scripts/Set_Value.py
@@ -1,8 +1,14 @@
-
 def main(value):
     """
     Function for returning a value to the WorkflowEngine
     :param value: Any value
     :return: The original input value
     """
+    if ',' in str(value) and isinstance(value,str):
+        values=[]
+        value=value.split(',')
+        for v in value:
+            values.append(v.strip())
+        value=values
     return value
+
