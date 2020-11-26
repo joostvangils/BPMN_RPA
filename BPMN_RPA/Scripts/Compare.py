5c43dace7699877f5adeda3d2f6fde9f2db28019
joostvangils <54207730+joostvangils@users.noreply.github.com>
2020-11-26 09:59:57 +0100

    Update Compare.py

--- BPMN_RPA/Scripts/Compare.py
+++ BPMN_RPA/Scripts/Compare.py
@@ -1,5 +1,5 @@
 
-def is_first_item_equal_to_second_item(first_item, second_item):
+def is_first_item_equal_to_second_item(first_item, second_item)-> bool:
     """
     Compare two items and check if they are equal
     :param first_item: The first item to compare
@@ -9,7 +9,7 @@ def is_first_item_equal_to_second_item(first_item, second_item):
     return first_item == second_item
 
 
-def is_first_item_less_than_second_item(first_item, second_item):
+def is_first_item_less_than_second_item(first_item, second_item)-> bool:
     """
     Check if first item is less than second item
     :param first_item: The first item to compare
@@ -23,7 +23,7 @@ def is_first_item_less_than_second_item(first_item, second_item):
     return first_item < second_item
 
 
-def is_first_item_greater_than_second_item(first_item, second_item):
+def is_first_item_greater_than_second_item(first_item, second_item)-> bool:
     """
     Check if first item is greater than second item
     :param first_item: The first item to compare
@@ -37,7 +37,7 @@ def is_first_item_greater_than_second_item(first_item, second_item):
     return first_item > second_item
 
 
-def is_first_item_less_or_equal_than_second_item(first_item, second_item):
+def is_first_item_less_or_equal_than_second_item(first_item, second_item)-> bool:
     """
     Check if first item is less or equal to second item
     :param first_item: The first item to compare
@@ -51,7 +51,7 @@ def is_first_item_less_or_equal_than_second_item(first_item, second_item):
     return first_item <= second_item
 
 
-def is_first_item_greater_or_equal_than_second_item(first_item, second_item):
+def is_first_item_greater_or_equal_than_second_item(first_item, second_item)-> bool:
     """
     Check if first item is greater or equal to second item
     :param first_item: The first item to compare
@@ -63,3 +63,13 @@ def is_first_item_greater_or_equal_than_second_item(first_item, second_item):
     if not isinstance(second_item, int) or not isinstance(second_item, float):
         raise Exception("The second item isn't a number")
     return first_item >= second_item
+
+def is_time_interval_less_or_equal(datetime1: Any, datetime2: Any, interval_in_seconds: int) -> bool:
+    """
+    Check if the interval between 2 date-times is less or equal than the given amount of seconds.
+    :param datetime1: The first datetime object
+    :param datetime2: The second datetime object
+    :param interval_in_seconds: The interval in seconds
+    :return: Boolean True or False
+    """
+    return (datetime2 - datetime1).total_seconds() <= interval_in_seconds
