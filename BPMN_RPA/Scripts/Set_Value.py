e58df9e89050857e140b24ca16faec560b315f88
joostvangils <54207730+joostvangils@users.noreply.github.com>
2020-12-08 09:52:21 +0100

    Update Set_Value.py

--- BPMN_RPA/Scripts/Set_Value.py
+++ BPMN_RPA/Scripts/Set_Value.py
@@ -1,4 +1,7 @@
-def value_to_variable(value):
+from typing import List, Any
+
+
+def value_to_variable(value: Any) -> Any:
     """
     Function for returning a value to the WorkflowEngine
     :param value: Any value
@@ -11,3 +14,13 @@ def value_to_variable(value):
             values.append(v.strip())
         value=values
     return value
+
+def split_string_to_list(string: str, separator: str = " ", maxsplit:int = -1) -> List:
+    """
+    Convert a string with to a list
+    :param string: The string to convert
+    :param separator: Optional. Specifies the separator to use when splitting the string. By default any whitespace is a separator.
+    :param maxsplit: Optional. Specifies how many splits to do. Default value is -1, which is "all occurrences".
+    :return: A list created from the string
+    """
+    return string.split(separator, maxsplit=maxsplit)
