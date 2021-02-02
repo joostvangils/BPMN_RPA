e955f7c070e755cd2db93f682ef2930dc4d2d07d
Joost van Gils <joostvangils@1ic.nl>
2021-02-02 20:24:42 +0100

    Added Visio shapes

--- BPMN_RPA/WorkflowEngine.py
+++ BPMN_RPA/WorkflowEngine.py
@@ -1477,7 +1477,7 @@ class Visio:
         return retn
 
 # Test
-engine = WorkflowEngine()
-doc = engine.open(fr"c:\\temp\\test2.vsdx")  # c:\\temp\\test.xml
-steps = engine.get_flow(doc)
-engine.run_flow(steps)
+# engine = WorkflowEngine()
+# doc = engine.open(fr"c:\\temp\\test2.vsdx")  # c:\\temp\\test.xml
+# steps = engine.get_flow(doc)
+# engine.run_flow(steps)
