fdc2398fdcd8f0881a4c990ea5a3c0dcde5cb1c6
JoostvanGils <joostvangils@1ic.nl>
2024-01-31 10:10:05 +0100

    Updated build procedure

--- Upload.bat
+++
@@ -1,6 +0,0 @@
-cd d:\Projects\BPMN_RPA
-Rmdir /S /Q "d:\Projects\BPMN_RPA\BPMN_RPA.egg-info"
-Rmdir /S /Q "d:\Projects\BPMN_RPA\build"
-Rmdir /S /Q "d:\Projects\BPMN_RPA\dist"
-python -m poetry build
-python -m twine upload --repository pypi dist/*
